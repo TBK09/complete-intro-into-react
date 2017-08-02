c30bfc1fcb8a9b31dcccbed98fbc7f8214e3ff42
Tobias Kocks <tobias.kocks@sqills.com>
2017-08-02 12:34:40 +0200

    - Create Details component for displaying details information of a show - Create Header component to refactor duplicate code - Move show retrieval logic from the Search component to the Router component in our ClientApp.js - Display the use of anonymous functions to pass parameters from one component to another - Display of how the Router component passes down route information to the route components

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom'
 import { BrowserRouter, Match } from 'react-router'
 import '../public/normalize.css'
 import '../public/style.css'
+import preload from '../public/data.json'
+import Details from './Details'
 import Landing from './Landing'
 import Search from './Search'
 
@@ -16,7 +18,17 @@ const App = React.createClass({
                  landing pages, as we don't want e.g. '/something/else' to also match the homepage pattern.
                  Using just the 'pattern' attribtue is more fuzzy.*/}
                 <Match exactly pattern='/' component={Landing} />
-                <Match pattern='/search' component={Search} />
+                <Match
+                    pattern='/search'
+                    component={(props) => <Search shows={preload.shows} {...props}/>} />
+                <Match
+                    pattern="/details/:id"
+                    component={(props) => {
+                        const shows = preload.shows.filter((show) => props.params.id === show.imdbID);
+
+                        return <Details show={shows[0]} {...props} />
+                    }}
+                />
             </div>
         </BrowserRouter>
     )
--- js/search.js
+++ js/search.js
@@ -1,21 +1,42 @@
 import React from 'react'
-import preload from '../public/data.json'
 import ShowCard from './showCard.js'
+const { arrayOf, shape, string } = React.PropTypes
 
 const Search = React.createClass({
+    propTypes: {
+        shows: arrayOf(shape({
+            title: string,
+            description: string
+        }))
+    },
+
+    getInitialState () {
+        return {
+            searchTerm : ''
+        }
+    },
+    handleSearchTermChange(event) {
+        this.setState({
+            searchTerm : event.target.value
+        })
+    },
     render () {
         return (
             <div className="search">
                 <header>
-                    <h1>svideo</h1>
-                    <input type="text" placeholder="Search"/>
+                    <h1>{this.state.searchTerm}</h1>
+                    <input onChange={this.handleSearchTermChange} type="text" placeholder={this.state.searchTerm}/>
                 </header>
                 <div>
-                    {preload.shows.map((show) => {
-                        return (
-                            <ShowCard key={show.imdbID} {...show}/>
-                        )
-                    })}
+                    {this.props.shows
+                        .filter((show) => {
+                            return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0;
+                        })
+                        .map((show) => {
+                            return (
+                                <ShowCard key={show.imdbID} {...show}/>
+                            )
+                        })}
                 </div>
             </div>
         )
